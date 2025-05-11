d4d3fd1598dff1aa55cb5fb65fe20a3cce1debb6
mathxam15 <matthewemiliano15@gmail.com>
2025-05-11 14:11:46 +0700

    fix link yt home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
                     </div>
                     <div class="swiper-slide">
                     <iframe class = "player" 
-                    src="www.youtube.com/embed/HDiADbPq4EA" 
+                    src="https://www.youtube.com/embed/HDiADbPq4EA" 
                     title="YouTube video player" 
                     frameborder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
