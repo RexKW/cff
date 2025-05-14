1bd68dfa4e887b4c992d6569f9f11e7298877763
mathxam15 <matthewemiliano15@gmail.com>
2025-05-14 21:00:36 +0700

    ASMARA ABIGAEL

--- resources/views/program/boundlessseries.blade.php
+++ resources/views/program/boundlessseries.blade.php
@@ -12,7 +12,7 @@
             </div>
             <div class = "col-12 col-md-7 align-middle">
                 <div class = "row my-2" data-aos = "fade-up" data-aos-duration = "1500" data-aos-delay = "250">
-                    <h1 class = "fw-bold fs-0 text-shadow"> EXPERT SESSION WITH <span class="btn-blue text-white p-2">asmara abigael</span> </h1>
+                    <h1 class = "fw-bold fs-0 text-shadow"> EXPERT SESSION WITH <span class="btn-blue text-white p-2">ASMARA ABIGAEL</span> </h1>
                 </div>
                 <h3 class = "fs-4 fw-bold" data-aos = "fade-up" data-aos-duration = "1500" data-aos-delay = "500"> DAY 4 | 30 MEI 2025 | 11.45 WIB </h3>
                 <p class = "fw-bold" data-aos = "fade-up" data-aos-duration = "1500" data-aos-delay = "750"> From Script To Screen: Navigating Actress’ Journey </p>
